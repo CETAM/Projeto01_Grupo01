e8c79876798b5b0fe6b81d455f8335fb49ed4b84
Cleverson S. Sobrinho <cleverson.silva@msn.com>
2026-08-12 15:46:58 -0400

    .

---
+++ src/main/java/cetam/projeto01grupo01/Projeto01Grupo01Application.java
@@ -0,0 +1,13 @@
+package cetam.projeto01grupo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01Grupo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01Grupo01Application.class, args);
+	}
+
+}
